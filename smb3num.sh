8395a2a666831c42af8ced44b8bd31eaa8d198c5
h1dz <85143916+h1dz@users.noreply.github.com>
2021-09-29 11:00:02 +1000

    Update smb3num.sh

--- smb3num.sh
+++ smb3num.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 #script for enumerating smb
-#chmod +x smb3num.sh
-#Usage:         ./smb3num <ip>
-
-
-echo 'IP = '$1 >> resultsSMB
-printf "\nRunning...\n\n"
-printf "\n----------------------------------Start------------------------------------------\n\n" >> resultsSMB
-printf "\n--------------------------------------------------------------------------------\n\n" >> resultsSMB
-printf "\n----------------------------------list smb--------------------------------------\n\n" >> resultsSMB
-smbclient -N -L //$1 >> resultsSMB
-printf "\n---------------------------------------------------------------------------------\n\n" >> resultsSMB
-printf "\n----------------------------------check writable-----------------------------\n\n" >> resultsSMB
-nmap -p 139,445 --script=smb-enum-shares.nse,smb-enum-users.nse $1 >> resultsSMB
-printf "\n---------------------------------------------------------------------------------\n\n" >> resultsSMB
-printf "\n----------------------------------crackmap-----------------------------------\n\n" >> resultsSMB
-crackmapexec smb $1 --shares >> resultsSMB
-printf "\n---------------------------------------------------------------------------------\n\n" >> resultsSMB
-printf "\n----------------------------------anon login----------------------------------\n\n" >> resultsSMB
-smbget -R smb://$1/anonymous >> resultsSMB
-printf "\n---------------------------------------------------------------------------------\n\n" >> resultsSMB
-printf "\n----------------------------------Finished------------------------------------\n\n" >> resultsSMB
-printf "\nShowing Results...\n\n"
+#usage:                       sudo ./smbenum <ip>
+B='\033[0;96m' #${B}
+G='\033[0;92m' #${G}
+R='\033[0;91m' #${R}
+Y='\033[0;33m' #${Y}
+N='\033[0m'    #${N}
+printf "${Y}\nScanning...\n\n" | tee -a -i smbResults
+date | tee -a -i smbResults
+printf ${Y}"\nIP = "$1 | tee -a -i smbResults
+printf "${G}\n----------------------------------Start----------------------------------\n\n" | tee -a -i smbResults
+printf "\n----------------------------------List SMB----------------------------------\n\n${R}" | tee -a -i smbResults
+smbclient -N -L //$1 | tee -a -i smbResults
+printf "${G}\n----------------------------------Check if writable----------------------------------\n\n${R}" | tee -a -i smbResults
+nmap -p 139,445 --script=smb-enum-shares.nse,smb-enum-users.nse $1 | tee -a -i smbResults
+smbmap -H $1 | tee -a -i smbResults
+printf "${G}\n----------------------------------Crackmap /OS----------------------------------\n\n${R}" | tee -a -i smbResults
+crackmapexec smb $1 --shares | tee -a -i smbResults
+printf "${G}\n----------------------------------Finished----------------------------------\n\n" | tee -a -i smbResults
+printf "${Y}\nAlso Try:...\n" | tee -a -i smbResults
+# \\\\ some slashes below are not showing
+printf ${Y}'\nConnect to root:       '${B}'smbclient \\\\$1\\wwwroot\n' | tee -a -i smbResults
+printf "${Y}\nConnect to Share w/ Anonymous:       "${B}"smbget -R smb://<ip>/anonymous    OR    smbclient //<ip>/<share> -U anonymous\n" | tee -a -i smbResults
+printf ${Y}'\nConnect to Share:       '${B}'smbclient \\\\<ip> \\<share>\n' | tee -a -i smbResults
+printf "${Y}\nConnect to Share with a Password:       "${B}"smbclient //<ip>/<share> -U<user>%'<password>'\n" | tee -a -i smbResults
 clear
-cat resultsSMB
+cat smbResults
