67875e1150393285749f1a8ac9af61ebedc06c4d
h1dz <85143916+h1dz@users.noreply.github.com>
2022-01-20 09:19:57 +1100

    Update smb3num.sh

--- smb3num.sh
+++ smb3num.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
-#script for enumerating smb
-#usage:                       sudo ./smbenum <ip>
+#Script for enumerating SMB port 445 with no credentials
+#Usage:                  sudo ./smb3num <ip>
 B='\033[0;96m' #${B}
 G='\033[0;92m' #${G}
 R='\033[0;91m' #${R}
 Y='\033[0;33m' #${Y}
 N='\033[0m'    #${N}
+rm -r -f smbResults 2>/dev/null
+clear
 printf "${Y}\nScanning...\n\n" | tee -a -i smbResults
 date | tee -a -i smbResults
 printf ${Y}"\nIP = "$1 | tee -a -i smbResults
 printf "${G}\n----------------------------------Start----------------------------------\n\n" | tee -a -i smbResults
-printf "\n----------------------------------List SMB----------------------------------\n\n${R}" | tee -a -i smbResults
-smbclient -N -L //$1 | tee -a -i smbResults
-printf "${G}\n----------------------------------Check if writable----------------------------------\n\n${R}" | tee -a -i smbResults
-nmap -p 139,445 --script=smb-enum-shares.nse,smb-enum-users.nse $1 | tee -a -i smbResults
-smbmap -H $1 | tee -a -i smbResults
-printf "${G}\n----------------------------------Crackmap /OS----------------------------------\n\n${R}" | tee -a -i smbResults
-crackmapexec smb $1 --shares | tee -a -i smbResults
-printf "${G}\n----------------------------------Finished----------------------------------\n\n" | tee -a -i smbResults
-printf "${Y}\nAlso Try:...\n" | tee -a -i smbResults
-# \\\\ some slashes below are not showing
-printf ${Y}'\nConnect to root:       '${B}'smbclient \\\\$1\\wwwroot\n' | tee -a -i smbResults
-printf "${Y}\nConnect to Share w/ Anonymous:       "${B}"smbget -R smb://<ip>/anonymous    OR    smbclient //<ip>/<share> -U anonymous\n" | tee -a -i smbResults
-printf ${Y}'\nConnect to Share:       '${B}'smbclient \\\\<ip> \\<share>\n' | tee -a -i smbResults
-printf "${Y}\nConnect to Share with a Password:       "${B}"smbclient //<ip>/<share> -U<user>%'<password>'\n" | tee -a -i smbResults
+printf "${G}\n----------------------------------Check if writable----------------------------------\n\n${B}" | tee -a -i smbResults
+sudo nmap -p445 --script=smb-enum-shares.nse,smb-enum-users.nse $1 2>/dev/null | tee -a -i smbResults
+sudo nmap -Pn -p445 --script=smb-enum-shares.nse,smb-enum-users.nse $1 2>/dev/null | tee -a -i smbResults
+printf "${G}\n----------------------------------CrackMapExec /OS----------------------------------\n\n${B}" | tee -a -i smbResults
+sudo crackmapexec smb $1 --shares 2>/dev/null | tee -a -i smbResults
+printf "\n----------------------------------List SMB----------------------------------\n\n${B}" | tee -a -i smbResults
+sudo smbclient -N -L //$1 2>/dev/null | tee -a -i smbResults
+sudo smbmap -H $1 2>/dev/null | tee -a -i smbResults 
+printf "\n----------------------------------Check Null Login----------------------------------\n\n${B}" | tee -a -i smbResults
+sudo smblient \\\\$1\\wwwroot 2>/dev/null | tee -a -i smbResults
+sudo smbget -R smb://$1/anonymous 2>/dev/null | tee -a -i smbResults
+sudo smbclient \\\\$1\\ -U Administrator 2>/dev/null | tee -a -i smbResults
+sudo smbget -R smb://$1/guest 2>/dev/null | tee -a -i smbResults
+sudo crackmapexec smb $1 -u '' -p '' 2>/dev/null | tee -a -i smbResults
+printf "${G}\n----------------------------------Finished----------------------------------\n\n${Y}" | tee -a -i smbResults
+date | tee -a -i smbResults
 clear
 cat smbResults
