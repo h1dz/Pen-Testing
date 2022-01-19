079ab1e730989f2a6f2f7aa4e40347a263001a9f
h1dz <85143916+h1dz@users.noreply.github.com>
2022-01-19 22:21:34 +1100

    Create ping.sh

---
+++ ping.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+var1=$(ping $1 -c 1 -W 3 | grep "64 bytes" | cut -d " " -f 6)
+if [[ "$var1" -eq "ttl=63" ]]; then
+echo "Host is LIVE! Most likely running: Linux OS"
+elif [[ "$var1" -eq "ttl=64" ]]; then
+echo "Host is LIVE! Most likely running: Linux OS"
+elif [[ "$var1" -eq "ttl=127" ]]; then
+echo "Host is LIVE! Most likely running: Windows OS"
+elif [[ "$var1" -eq "ttl=128" ]]; then
+echo "Host is LIVE! Most likely running: Windows OS"
+elif [[ "$var1" -eq "ttl=31" ]]; then
+echo "Host is LIVE! Most likely running: Windows 95 OS"
+elif [[ "$var1" -eq "ttl=32" ]]; then
+echo "Host is LIVE! Most likely running: Windows 95 OS"
+elif [[ "$var1" == "" ]]; then
+echo "Host appears to be DOWN!"
+fi
