3be872495a220483771d3eb45257595ab1fa74c7
h1dz <85143916+h1dz@users.noreply.github.com>
2022-03-20 22:39:27 +1100

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -5,13 +5,15 @@ echo "Host is LIVE! Most likely running: Linux OS"
 elif [[ "$var1" -eq "ttl=64" ]]; then
 echo "Host is LIVE! Most likely running: Linux OS"
 elif [[ "$var1" -eq "ttl=127" ]]; then
-echo "Host is LIVE! Most likely running: Windows OS"
+tput setaf 6; echo "Host is LIVE! Most likely running: Windows OS"
 elif [[ "$var1" -eq "ttl=128" ]]; then
-echo "Host is LIVE! Most likely running: Windows OS"
+tput setaf 6; echo "Host is LIVE! Most likely running: Windows OS"
 elif [[ "$var1" -eq "ttl=31" ]]; then
 echo "Host is LIVE! Most likely running: Windows 95 OS"
 elif [[ "$var1" -eq "ttl=32" ]]; then
 echo "Host is LIVE! Most likely running: Windows 95 OS"
+elif [[ "$var1" -eq "ttl=61" ]]; then
+echo "Host is LIVE! Most likely running: Linux OS"
 elif [[ "$var1" == "" ]]; then
 echo "Host appears to be DOWN!"
 fi
