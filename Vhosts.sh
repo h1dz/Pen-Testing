299ce10f31c9c2dddb3082c62fe8a78e2ff9d58e
h1dz <85143916+h1dz@users.noreply.github.com>
2022-04-24 00:09:32 +1000

    Update Vhosts.sh

--- Vhosts.sh
+++ Vhosts.sh
@@ -8,10 +8,11 @@ D='\033[5;35m' #${D}
 N='\033[0m'    #${N}
 rm -rf GobusterVhost_DUMP.txt 2>/dev/null
 date | tee -a -i GobusterVhost_DUMP.txt
-printf "${B}\n\n----------------------------------Finished Directory Scans----------------------------------\n\n${G}" | tee -a -i GobusterVhost_DUMP.txt
-printf "${B}\n\n----------------------------------Starting VHOST Scan----------------------------------\n\n${G}" | tee -a -i GobusterVhost_DUMP.txt
+printf "${B}\n\n----------------------------------Starting VHOST Scans----------------------------------\n\n${G}" | tee -a -i GobusterVhost_DUMP.txt
     gobuster vhost --url $1 --wordlist /usr/share/wordlists/seclists/Discovery/DNS/fierce-hostlist.txt 2>/dev/null | tee -a -i GobusterVhost_DUMP.txt
+    printf "${B}\n\n--------------------------------------------------------------------\n\n${G}" | tee -a -i GobusterVhost_DUMP.txt
     gobuster vhost --url $1 --wordlist /usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-110000.txt 2>/dev/null | tee -a -i GobusterVhost_DUMP.txt
+    printf "${B}\n\n--------------------------------------------------------------------\n\n${G}" | tee -a -i GobusterVhost_DUMP.txt
     gobuster vhost --url $1 --wordlist /usr/share/wordlists/seclists/Discovery/DNS/dns-Jhaddix.txt 2>/dev/null | tee -a -i GobusterVhost_DUMP.txt
 printf "${B}\n\n----------------------------------Finished VHOST Scans----------------------------------\n\n${Y}" | tee -a -i GobusterVhost_DUMP.txt
 date | tee -a -i GobusterVhost_DUMP.txt
