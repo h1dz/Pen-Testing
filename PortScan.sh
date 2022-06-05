94825162070b47647036a36d8a12d2d1c0b901d4
h1dz <85143916+h1dz@users.noreply.github.com>
2022-06-05 22:53:30 +1000

    Update PortScan.sh

--- PortScan.sh
+++ PortScan.sh
@@ -4,9 +4,10 @@ rm -rf masscan.txt 2>/dev/null
 rm -rf ports.txt 2>/dev/null
 date | tee -a -i ports.txt
 tput sgr0; echo ""
-sudo nmap -Pn -F --min-rate 100 $1 -T5 --open
-sudo masscan  -e tun0 -p1-65535,U:1-65535 --open-only --interactive --rate=500 $1 | tee -a -i masscan.txt
-ports=$(cat masscan.txt | sed s/'Discovered open port //' | awk -F/ '{print $1}' | awk '!seen[$0]++' ORS=',' | sort); sudo nmap -Pn -sV -sC -p$ports --min-rate 100 $1 | tee -a -i ports.txt
+sudo nmap -Pn -F --min-rate 100 $1 -T4 --open
+sudo masscan  -e tun0 -p1-65535 --open-only --interactive --rate=500 $1 | tee -a -i masscan.txt
+ports=$(cat masscan.txt | sed s/'Discovered open port //' | awk -F/ '{print $1}' | awk '!seen[$0]++' ORS=',' | sort); sudo nmap -Pn -sV -sC -p$ports --min-rate 100 $1 -T4 | tee -a -i ports.txt
+sudo nmap -Pn -sV -sU -p- --min-rate 100 $1 -T4 | tee -a -i ports.txt
 date | tee -a -i ports.txt
 clear
 cat ports.txt 2>/dev/null
