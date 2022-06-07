cdf979bcf6dff74890de4e993751bc7defa9074b
h1dz <85143916+h1dz@users.noreply.github.com>
2022-06-07 15:43:16 +1000

    Update PortScan.sh

--- PortScan.sh
+++ PortScan.sh
@@ -7,6 +7,7 @@ tput sgr0; echo ""
 sudo nmap -Pn -F --min-rate 100 $1 -T4 --open
 sudo masscan  -e tun0 -p1-65535 --open-only --interactive --rate=500 $1 | tee -a -i masscan.txt
 ports=$(cat masscan.txt | sed s/'Discovered open port //' | awk -F/ '{print $1}' | awk '!seen[$0]++' ORS=',' | sort); sudo nmap -Pn -sV -sC -p$ports --min-rate 100 $1 -T4 | tee -a -i ports.txt
+sudo nmap -sU -F --min-rate 100 $1 -T4 --open
 sudo nmap -Pn -sV -sU -p- --min-rate 100 $1 -T4 | tee -a -i ports.txt
 date | tee -a -i ports.txt
 clear
