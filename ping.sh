a33214a09a5568b24006193d913119e47d214968
h1dz <85143916+h1dz@users.noreply.github.com>
2022-06-09 10:42:57 +1000

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
+G='\033[5;32m' #${G} green
+R='\033[5;31m' #${R} red
+P='\033[4;35m' #${P} purple
+N='\033[0m'    #${N} null
 var1=$(ping $1 -c 1 -W 3 | grep "64 bytes" | cut -d " " -f 6)
 if [[ "$var1" -eq "ttl=63" ]]; then
-echo "Host is LIVE! Most likely running: Linux OS"
+printf "Host is ${G}LIVE!${N} Most likely running: ${P}Linux OS${N}"
 elif [[ "$var1" -eq "ttl=64" ]]; then
-echo "Host is LIVE! Most likely running: Linux OS"
+printf "Host is ${G}LIVE!${N} Most likely running: ${P}Linux OS${N}"
 elif [[ "$var1" -eq "ttl=127" ]]; then
-echo "Host is LIVE! Most likely running: Windows OS"
+printf "Host is ${G}LIVE!${N} Most likely running: ${P}Windows OS${N}"
 elif [[ "$var1" -eq "ttl=128" ]]; then
-echo "Host is LIVE! Most likely running: Windows OS"
+printf "Host is ${G}LIVE!${N} Most likely running: ${P}Windows OS${N}"
 elif [[ "$var1" -eq "ttl=31" ]]; then
-echo "Host is LIVE! Most likely running: Windows 95 OS"
+printf "Host is ${G}LIVE!${N} Most likely running: ${P}Windows 95 OS${N}"
 elif [[ "$var1" -eq "ttl=32" ]]; then
-echo "Host is LIVE! Most likely running: Windows 95 OS"
+printf "Host is ${G}LIVE!${N} Most likely running: ${P}Windows 95 OS${N}"
 elif [[ "$var1" -eq "ttl=255" ]]; then
-echo "Host is LIVE! Most likely running: MAC / OPENBSD / CISCO / SOLARIS OS"
+printf "Host is ${G}LIVE!${N} Most likely running: ${P}MAC / OPENBSD / CISCO / SOLARIS OS${N}"
 elif [[ "$var1" -eq "ttl=254" ]]; then
-echo "Host is LIVE! Most likely running: MAC / OPENBSD / CISCO / SOLARIS OS"
+printf "Host is ${G}LIVE!${N} Most likely running: ${P}MAC / OPENBSD / CISCO / SOLARIS OS${N}"
 elif [[ "$var1" == "" ]]; then
-echo "Host appears to be DOWN!"
+printf "Host appears to be ${R}DOWN!${N}"
 fi
