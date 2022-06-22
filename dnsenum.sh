9fc9ef2934868268d786c40fcd95f2efc8c8e1ac
h1dz <85143916+h1dz@users.noreply.github.com>
2022-06-22 11:27:43 +1000

    Update dnsenum.sh

--- dnsenum.sh
+++ dnsenum.sh
@@ -19,13 +19,8 @@ host $2 2>/dev/null
 host -l $2 $1 2>/dev/null
 traceroute $1 2>/dev/null
 fierce --domain $2 2>/dev/null
-#for ip in $(seq 1 254); do host 10.10.10.$ip; done | grep -v "not found"     #change 10.10.10. to first 3 octets of RHOST
-for server in $(host -t ns $2 | cut -d " " -f4); do host -l $2 $server | grep "has address"; done 2>/dev/null
 subfinder -d $2 2>/dev/null
 amass enum -d $2 2>/dev/null
-echo "Bruteforcing subdomains..." 
-wfuzz -c -w /usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1million-110000.txt -u http://$2 -v -H "Host:FUZZ.$2" 2>/dev/null
-gobuster vhost -w /usr/share/wordlists/seclists/Discovery/DNS/subdomains-top1mil-5000.txt -u $1 -k 2>/dev/null
 echo "Try reverse-dns: https://whois.domaintools.com"
 echo "Bruteforcing domains, will take a while..."
 dnsrecon -d $2 -D /usr/share/wordlists/seclists/Discovery/DNS/dns-Jhaddix.txt -t brt 2>/dev/null
